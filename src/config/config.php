fdcf77d4130df4823c9724a212cc4ba3152fff29
Rachid Rafia <rrafia@outlook.com>
2015-01-09 19:06:31 -0500

    fix config file

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'public_key'     =>  '6Ld-QQATAAAAAPOtG7TzFl4P41RDY2m58rQDKjEm',
-    'private_key'    =>  '6Ld-QQATAAAAAGgknxdgMcVLfDdzkZLlLkRMpthV',
+    'public_key'     =>  '',
+    'private_key'    =>  '',
     'verify_api_url'    =>  'https://www.google.com/recaptcha/api/siteverify',
     'error_message'    =>  '<span style="color:red">There was an error with recaptcha</span>'
 ];
